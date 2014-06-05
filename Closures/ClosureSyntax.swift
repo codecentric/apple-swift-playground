f128c49eed8356773660144a1d45abfaa52865b5
Nicolas Neubauer <nicolas.neubauer@codecentric.de>
2014-06-05 23:18:43 +0200

    It's always good to double check what you are commiting...

--- Closures/ClosureSyntax.swift
+++ Closures/ClosureSyntax.swift
@@ -1,33 +1,37 @@
+/* 
+ * Different kinds of syntax for passing a closure to a function.
+ * Provided by aminmeyghani.
+ */
+
 // function that takes in a function.
-func makerAdder (a:Int, b:Int, fn:(Int,Int)->Int) -> Int {
-  return fn(a,b)
+func returnResultOfClosure (a:Int, b:Int, fn:(Int,Int)->Int) -> Int {
+    return fn(a,b)
 }
 
-// Different kinds of syntax for passing a closure to a function.
-// --------------------------
 // very verbose
-var verbose = makerAdder(1, 5, { (a:Int, b:Int)-> Int in return a+b
-  })
+var verbose = returnResultOfClosure(1, 5, { (a:Int, b:Int)-> Int in return a+b })
+println(verbose)
+
+// you can also provide the closure after the method parentheses if it is the last parameter
+var verbose2 = returnResultOfClosure(1, 5) { (a:Int, b:Int)-> Int in return a+b }
 println(verbose)
 
 // less verbose
-var lessV = makerAdder(1, 8, { (a:Int, b:Int)in return a+b  })
-println(lessV)
+var lessVerbose = returnResultOfClosure(1, 8, { (a:Int, b:Int) in return a+b })
+println(lessVerbose)
 
-// less verbose2
-var lessV2 = makerAdder(1, 3, { (a, b)in
-  return a+b
-  })
-println(lessV2)
+// less verbose 2
+var lessVerbose2 = returnResultOfClosure(1, 3, { (a, b) in return a+b })
+println(lessVerbose2)
 
-// less verbose3
-var lessV3 = makerAdder(1, 15, { (a, b) in a+b  })
-println(lessV3)
+// less verbose 3
+var lessVerbose3 = returnResultOfClosure(1, 15, { (a, b) in a+b })
+println(lessVerbose3)
 
-// less verbose4
-var lessV4 = makerAdder(1, 14, { $0 + $1  })
-println(lessV4)
+// less verbose 4
+var lessVerbose4 = returnResultOfClosure(1, 14, { $0 + $1 })
+println(lessVerbose4)
 
-// less verbose4
-var lessV5 = makerAdder(2, 16, +)
-println(lessV5)
+// less verbose 5
+var lessVerbose5 = returnResultOfClosure(2, 16, +)
+println(lessVerbose5)
